f0ad255155590442411b3ce595e8204dec8dc4cf
Ashley Sheridan <ash@ashleysheridan.co.uk>
2025-02-05 22:48:41 +0000

    added abilities and modifier values to character resource response

--- app/Http/Resources/CharacterResource.php
+++ app/Http/Resources/CharacterResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\CharAbility;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -19,8 +21,36 @@ class CharacterResource extends JsonResource
                 'characteristics' => CharBackgroundCharacteristicResource::collection($this->CharacterBackgroundCharacteristics) ?? [],
             ],
             'charRace' => $this->CharacterRace->name ?? '',
+            'abilities' => $this->parseAbilities($this->abilities),
             'custom_portrait' => $this->whenLoaded('custom_portrait'),
             'created_at' => $this->created_at,
         ];
+    }
+
+    private function parseAbilities($abilitiesJsonStr): Collection
+    {
+        $abilities = CharAbility::all();
+
+        try {
+            $abilitiesJson = json_decode($abilitiesJsonStr, true);
+
+            foreach ($abilities as &$ability)
+            {
+                if (isset($abilitiesJson[$ability->short_name]))
+                {
+                    $ability->value = $abilitiesJson[$ability->short_name];
+                    $ability->modifier = floor(($ability->value - 10) / 2);
+                }
+                else
+                {
+                    $ability->value = 0;
+                    $ability->modifier = 0;
+                }
+            }
+        } catch (\Exception $e) {
+
+        }
+
+        return $abilities;
     }
 }
