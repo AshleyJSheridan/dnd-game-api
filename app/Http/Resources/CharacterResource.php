3f7b68428b5e714385edf101fbdd17b845af7f91
Ashley Sheridan <ash@ashleysheridan.co.uk>
2025-02-06 20:52:10 +0000

    added ability racial modifiers to character response

--- app/Http/Resources/CharacterResource.php
+++ app/Http/Resources/CharacterResource.php
@@ -36,21 +36,39 @@ class CharacterResource extends JsonResource
 
             foreach ($abilities as &$ability)
             {
+                $ability->base = 0;
+
                 if (isset($abilitiesJson[$ability->short_name]))
                 {
-                    $ability->value = $abilitiesJson[$ability->short_name];
-                    $ability->modifier = floor(($ability->value - 10) / 2);
+                    $ability->base = $abilitiesJson[$ability->short_name];
                 }
-                else
-                {
-                    $ability->value = 0;
-                    $ability->modifier = 0;
-                }
+
+                $ability->racialModifier = $this->getRacialModifier($ability->id);
+
+                $ability->modifier = floor(($ability->base + $ability->racialModifier - 10) / 2);
             }
         } catch (\Exception $e) {
-
+            var_dump($e->getMessage());
         }
 
         return $abilities;
+    }
+
+    private function getRacialModifier(int $abilityId): int
+    {
+        // local version of Maria doesn't have newer JSON_* methods, so doing this in less than ideal way
+        $traits = $this->CharacterRace->RaceTraits
+            ->where('type', 'ability_increase');
+
+        foreach ($traits as $trait)
+        {
+            $ability_details = json_decode($trait->ability_details);
+
+            // we only care about the first match, there shouldn't be two modifiers here for the same ability
+            if (in_array($abilityId, $ability_details->abilities))
+                return $ability_details->increase;
+        }
+
+        return 0;
     }
 }
