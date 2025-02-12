e3e1a6f683075e43e61555331390de97204862b0
Ashley Sheridan <ash@ashleysheridan.co.uk>
2025-02-12 05:48:19 +0000

    simplified item service class initialisation to remove switch/case

--- app/Http/Factories/ItemFactory.php
+++ app/Http/Factories/ItemFactory.php
@@ -2,20 +2,15 @@
 
 namespace App\Http\Factories;
 
-use App\Services\Items\WeaponItemService;
 use Illuminate\Support\Facades\App;
-use App\Services\Items\BookItemService;
 
 class ItemFactory
 {
     public static function create(string $itemType)
     {
-        switch ($itemType)
-        {
-            case 'book':
-                return App::make(BookItemService::class);
-            case 'weapon':
-                return App::make(WeaponItemService::class);
-        }
+        $classPath = '\\App\\Services\\Items\\' . ucfirst($itemType) . 'ItemService';
+
+        if (class_exists($classPath))
+            return App::make($classPath);
     }
 }
